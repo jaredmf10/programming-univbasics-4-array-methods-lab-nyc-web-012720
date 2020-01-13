c6eac10bf8f3beb1885e22fb10ace953cc195327
Jared Fleming <jaredmf10@gmail.com>
2020-01-13 18:47:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,19 +4,23 @@ array.include("wow")
 end
 
 def using_sort(array)
-
+array = ["wow", 1, 2, 3, "arrays"]
+array.sort
 end
 
 def using_reverse(array)
-
+array = ["wow", 1, 2, 3, "arrays"]
+array.reverse
 end
 
 def using_first(array)
-
+array = ["wow", 1, 2, 3, "arrays"]
+array.first
 end
 
 def using_last(array)
-
+array = ["wow", 1, 2, 3, "arrays"]
+array.last
 end
 
 def using_size(array)
